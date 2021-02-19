c649968df651ffdf3f4af1c1bc608b364dec8762
X1 Carbon <mahiich@mail.ru>
2021-02-19 09:03:43 +0500

    added addEditSaveStudent.html, getstudents.html and some changes

--- src/main/java/uz/mahiich/controllers/StudentController.java
+++ src/main/java/uz/mahiich/controllers/StudentController.java
@@ -2,14 +2,23 @@ package uz.mahiich.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/views")
+@RequestMapping("/students")
 public class StudentController {
 
-    @GetMapping("/index.html")
+    @GetMapping("/getstudents")
     public String getStudents(){
-        return "index";
+        return "/students/getstudents";
     }
+
+    @GetMapping("/addstudent")
+    public String addStudent(){
+        return "/students/addEditSaveStudent";
+    }
+
+
+
 }
